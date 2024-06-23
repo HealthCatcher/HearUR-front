5d20f8d4541e7cfc0e63ba995eb51b7d0c6f5c36
BiYeon <isly7959@naver.com>
2024-06-23 15:35:04 +0900

    chore: 하단 네비게이션 바 형식 변경

--- src/components/common/bottomnav/bottomnav.tsx
+++ src/components/common/bottomnav/bottomnav.tsx
@@ -4,37 +4,41 @@ import Link from 'next/link';
 import {usePathname} from 'next/navigation';
 
 const NavigatorContainer = styled.div`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  background-color: #f0f0f0;
-  height: 60px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: #f0f0f0;
+    height: 60px;
 `;
 
 const NavButton = styled.button`
-  padding: 10px 20px;
-  background-color: #4CAF50;
-  color: white;
-  border: none;
-  cursor: pointer;
-  transition: background-color 0.3s ease;
+    padding: 10px 20px;
+    background-color: #4CAF50;
+    color: white;
+    border: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
 
-  &:hover {
-    background-color: #ddd;
-  }
+    &:hover {
+        background-color: #ddd;
+    }
 `;
 
 const BottomNavigator = () => {
   const path = usePathname();
+
   return (
-    <NavigatorContainer>
-      <NavButton><Link href={"/health-information"}>건강정보</Link></NavButton>
-      <NavButton><Link href={"/self-diagnosis"}>자가진단</Link></NavButton>
-      <NavButton><Link href={"/experience-notice"}>체험단</Link></NavButton>
-      <NavButton><Link href={"/community"}>커뮤니티</Link></NavButton>
-      <NavButton><Link href={"/my"}>내정보</Link></NavButton>
-    </NavigatorContainer>
+      <div>
+        <NavigatorContainer>
+          <Link href={"/health-information"}><NavButton>건강정보</NavButton></Link>
+          <Link href={"/self-diagnosis"}><NavButton>자가진단</NavButton></Link>
+          <Link href={"/experience-notice"}><NavButton>체험단</NavButton></Link>
+          <Link href={"/community"}><NavButton>커뮤니티</NavButton></Link>
+          <Link href={"/my"}><NavButton>내정보</NavButton></Link>
+        </NavigatorContainer>
+      </div>
   );
+
 };
 
 export default BottomNavigator;
