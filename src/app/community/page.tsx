66af8c1cdd5e69bbdcbc8b3313679602b8bedfec
byun0-0 <isly7959@naver.com>
2024-07-03 20:15:00 +0900

    feat: 게시글 작성 페이지 추가

--- src/app/community/page.tsx
+++ src/app/community/page.tsx
@@ -1,11 +1,13 @@
+import Link from "next/link";
+
 const CommunityPage = () => {
   return (
       <>
         <div>
           <h1>Community Page</h1>
-          <section className={"community-cover"}>
+          <section className={"community-cover bg-amber-200 pt-8 pb-10 pl-10"}>
             <div className={"cover-content"}>
-              <h1 className={"text-4xl font-bold"}>커뮤니티</h1>
+              <h1 className={"text-4xl font-bold pb-10"}>커뮤니티</h1>
               <p className={"text-lg"}>사람들과 소통하고 정보를 공유해보세요.</p>
             </div>
             <span className={"cover-description"}>
@@ -13,9 +15,9 @@ const CommunityPage = () => {
             </span>
           </section>
           <section className={"community-post-list"}>
-            <div className={"post-list-header"}>
-              <h2>최신 글</h2>
-              <button className={"post-write-button"}>글쓰기</button>
+            <div className={"post-list-header flex justify-between items-center post-list-header ml-10 mr-5 mt-2 mb-2"}>
+              <div className={"text-lg font-bold"}>최신 글</div>
+              <Link className={"post-write-button text-black px-4 py-2 rounded hover:bg-blue-200 border"} href={"/community/write"}>글쓰기</Link>
             </div>
           </section>
         </div>
