b2db414e94fe0464d6bd73112faeb3e1d4744a57
byun0-0 <isly7959@naver.com>
2024-07-07 02:40:01 +0900

    feat: 게시글 작성 페이지 api 연결

--- src/app/community/write/page.tsx
+++ src/app/community/write/page.tsx
@@ -1,36 +1,67 @@
 "use client";
-import React, { useState } from 'react';
+import React, {useEffect, useState} from 'react';
+import {useRouter} from 'next/navigation';
+import axios from "axios";
 
 const WritePage = () => {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  const [category, setCategory] = useState('');
+  const router = useRouter();
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        // 여기서 폼 데이터를 처리하거나 API 호출 등의 작업을 수행할 수 있습니다.
-        console.log('제목:', title);
-        console.log('내용:', content);
-        // 예시로 console에 출력하는 것으로 대체합니다.
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('jwt') === null) {
+        router.push('/login');
+      }
     }
+  }, []);
+  const handleSubmit = () => {
+    axios.post('http://localhost:8080/api/v1/community/post', {
+      title: title,
+      content: content,
+      category: category,
+    }, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('jwt'),
+      }
+    }).then(r => {
+      console.log(r);
+      console.log(r.headers);
+      if (r.status === 201) router.push(r.headers['location']);
+      else throw new Error('글쓰기 실패');
+    }).catch(e => {
+      alert(e.message);
+    })
+  }
 
-    return (
-        <div className="max-w-2xl mx-auto mt-8">
-            <h1 className="text-2xl font-bold mb-4">글쓰기</h1>
-            <form onSubmit={handleSubmit} className="space-y-4">
-                <div>
-                    <label htmlFor="title" className="block text-sm font-medium text-gray-700">제목</label>
-                    <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)} className="mt-1 px-3 py-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm" />
-                </div>
-                <div>
-                    <label htmlFor="content" className="block text-sm font-medium text-gray-700">내용</label>
-                    <textarea id="content" value={content} onChange={(e) => setContent(e.target.value)} rows={5} className="mt-1 px-3 py-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></textarea>
-                </div>
-                <div>
-                    <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">글쓰기</button>
-                </div>
-            </form>
+  return (
+      <div className="max-w-2xl mx-auto pt-8">
+        <h1 className="text-2xl font-bold mb-4">글쓰기</h1>
+        <div>
+          <label htmlFor="title" className="block text-sm font-medium text-gray-700">제목</label>
+          <input type="text" id="title" value={title} onChange={(e) => setTitle(e.target.value)}
+                 className="mt-1 px-3 py-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"/>
         </div>
-    );
+        <div>
+          <label htmlFor="category" className="block text-sm font-medium text-gray-700">카테고리</label>
+          <input type="text" id="category" value={category} onChange={(e) => setCategory(e.target.value)}
+                 className="mt-1 px-3 py-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"/>
+        </div>
+        <div>
+          <label htmlFor="content" className="block text-sm font-medium text-gray-700">내용</label>
+          <textarea id="content" value={content} onChange={(e) => setContent(e.target.value)} rows={5}
+                    className="mt-1 px-3 py-2 block w-full border border-gray-300 rounded-md focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"></textarea>
+        </div>
+        <div>
+          <button onClick={handleSubmit}
+                  className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700">글쓰기
+          </button>
+        </div>
+
+      </div>
+  );
 }
 
 export default WritePage;
