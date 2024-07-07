0b564a15e637549ed8f5c831cacc8f8b05061b17
byun0-0 <isly7959@naver.com>
2024-07-07 15:10:48 +0900

    feat: 내 정보 페이지 추가

--- src/app/my/page.tsx
+++ src/app/my/page.tsx
@@ -1,9 +1,25 @@
-const myPage = () =>{
-    return (
-        <div>
-            <h1>Hello!</h1>
+import Logout from "@/app/my/Logout";
+import NickName from "@/app/my/NickName";
+import NewPassword from "@/app/my/NewPassword";
+import IsLoggedIn from "@/app/my/IsLoggedIn";
+
+export const metadata = {
+  title: "HearUR | 내 정보",
+};
+
+const Page = () => {
+
+  return (
+      <div className="bg-gray-100 flex items-center justify-center h-screen">
+        <IsLoggedIn/>
+        <div className="bg-white p-8 rounded shadow-lg max-w-md w-full">
+          <h1 className="text-4xl font-bold mb-4">내 정보</h1>
+          <Logout/>
+          <NewPassword/>
+          <NickName/>
         </div>
-    );
+      </div>
+  );
 }
 
-export default myPage;
+export default Page;
