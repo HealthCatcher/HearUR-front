6a8716245dff0683dd2bab08468c38f93bff5351
byun0-0 <isly7959@naver.com>
2024-07-07 12:50:51 +0900

    refactor: router를 매개변수로 전달

--- src/app/signup/SignUp.tsx
+++ src/app/signup/SignUp.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, {useState} from "react";
 import fetchSignUp from "@/app/signup/fetchSignUp";
+import {useRouter} from "next/navigation";
 
 const validateEmail = (email: string) => {
   // 이메일 형식의 정규 표현식
@@ -9,33 +10,34 @@ const validateEmail = (email: string) => {
 };
 
 const SignUp = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [name, setName] = useState("");
-  const [passwordError, setPasswordError] = useState("");
+  const router = useRouter();
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [name, setName] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
   const handleSignup = () => {
     console.log("handle signup");
     if (!validateEmail(username)) {
-      setPasswordError("이메일 형식이 올바르지 않습니다.");
+      setErrorMessage("이메일 형식이 올바르지 않습니다.");
       return;
     }
     if (password.length < 8) {
-      setPasswordError("비밀번호는 8자 이상이어야 합니다.");
+      setErrorMessage("비밀번호는 8자 이상이어야 합니다.");
       return;
     }
     if (password !== confirmPassword) {
-      setPasswordError("비밀번호가 일치하지 않습니다.");
+      setErrorMessage("비밀번호가 일치하지 않습니다.");
       return;
     }
     const specialCharacterPattern = /[!@#$%^&*(),.?":{}|<>]/;
     if (!specialCharacterPattern.test(password)) {
-      setPasswordError("비밀번호에 특수문자를 포함해야 합니다.")
+      setErrorMessage("비밀번호에 특수문자를 포함해야 합니다.")
       return;
     }
     // 이후 회원가입 로직 추가
-    setPasswordError(""); // 에러 메시지 초기화
-    fetchSignUp(username, name, password); // 실제 회원가입 함수 호출
+    setErrorMessage(""); // 에러 메시지 초기화
+    fetchSignUp(setErrorMessage, router, username, name, password)
   };
   return (
       <>
@@ -64,7 +66,7 @@ const SignUp = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
         />
         {
-            passwordError && <p className="text-red-500 mb-4">{passwordError}</p>
+            errorMessage && <p className="text-red-500 mb-4">{errorMessage}</p>
         }
         <button
             className="w-80 h-10 bg-blue-500 text-white font-bold rounded-lg mb-2 hover:bg-blue-600"
--- src/app/signup/fetchSignUp.ts
+++ src/app/signup/fetchSignUp.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
-import {useRouter} from "next/navigation";
+import {AppRouterInstance} from "next/dist/shared/lib/app-router-context.shared-runtime";
+import {Dispatch} from "react";
 
-const onSignup = (username: string, name: string, password: string) => {
-  const router = useRouter();
+const onSignup = (setErrorMessage: Dispatch<any>, router: AppRouterInstance, username: string, name: string, password: string) => {
   axios.post('http://localhost:8080/api/v1/auth/signup', {
     username: username,
     password: password,
@@ -14,18 +14,18 @@ const onSignup = (username: string, name: string, password: string) => {
   })
       .then(response => {
         console.log("response: ", response);
-        if (response.status === 201){
-          router.push("/login");
-          return null;
-        }
-        else throw new Error("회원가입 요청에 문제가 생겼습니다.");
+        if (response.status === 201) {
+          alert("회원가입이 완료되었습니다.");
+          router.push('/login');
+        } else throw new Error("회원가입 요청에 문제가 생겼습니다.");
       })
       .catch(error => {
         console.log(error);
-        if (error.response.data.status_code == "400") {
-          alert("이메일이 중복되었습니다.");
-        } else alert("회원가입 요청에 문제가 생겼습니다.");
-        return null;
+        if (error.response.status == "400") {
+          setErrorMessage("이메일이 중복되었습니다.");
+        } else {
+          setErrorMessage("회원가입 요청에 문제가 생겼습니다. 다시 시도해주세요.");
+        }
       });
 };
 
