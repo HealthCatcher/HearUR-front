3abcbeb4edc7525eed29b076d747801ea5a57bbf
byun0-0 <isly7959@naver.com>
2024-07-20 01:09:47 +0900

    feat: 추천 api 추가

---
+++ src/utils/api/post/addLike.ts
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+export const addLike = async (postNo: number) => {
+  if (localStorage.getItem('jwt') === null) {
+    alert('로그인이 필요한 기능입니다.');
+    return
+  }
+  return axios.post(`http://localhost:8080/api/v1/community/post/${postNo}/like`, {}, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${localStorage.getItem('jwt')}`,
+    }
+  }).then(response => {
+    if (response.status === 204) {
+      return response.data;
+    } else {
+      throw new Error("좋아요를 추가하는데 문제가 생겼습니다.");
+    }
+  }).catch(error => {
+    console.error('Error: ', error);
+    return [];
+  });
+}
