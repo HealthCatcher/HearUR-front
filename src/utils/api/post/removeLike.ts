687d7d769fdc5f7470c2f7595e67345e804857fc
byun0-0 <isly7959@naver.com>
2024-07-21 18:52:12 +0900

    style: 들여쓰기 수정

--- src/utils/api/post/removeLike.ts
+++ src/utils/api/post/removeLike.ts
@@ -1,19 +1,19 @@
 import axios from 'axios'
 
 export const removeLike = async (postNo: number) => {
-    return axios.delete(`http://localhost:8080/api/v1/community/post/${postNo}/like`, {
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `${localStorage.getItem('jwt')}`,
-        }
-    }).then(response => {
-        if (response.status === 204) {
-            return response.data;
-        } else {
-            throw new Error("좋아요를 삭제하는데 문제가 생겼습니다.");
-        }
-    }).catch(error => {
-        console.error('Error: ', error);
-        return [];
-    });
+  return axios.delete(`http://localhost:8080/api/v1/community/post/${postNo}/like`, {
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': `${localStorage.getItem('jwt')}`,
+    }
+  }).then(response => {
+    if (response.status === 204) {
+      return response.data;
+    } else {
+      throw new Error("좋아요를 삭제하는데 문제가 생겼습니다.");
+    }
+  }).catch(error => {
+    console.error('Error: ', error);
+    return -1;
+  });
 }
