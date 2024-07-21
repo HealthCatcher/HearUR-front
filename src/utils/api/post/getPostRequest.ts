6ba5aadaeb0c0f9ff0fe70fd910d38274c411c51
byun0-0 <isly7959@naver.com>
2024-07-21 18:52:00 +0900

    refactor: type정의

--- src/utils/api/post/getPostRequest.ts
+++ src/utils/api/post/getPostRequest.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
+import {Post} from "@/components/post/PostItem";
 
-const getPostRequest = async (no: number) => {
+const getPostRequest = async (no: number): Promise<Post | number> => {
   return axios.get(`http://localhost:8080/api/v1/community/post/${no}`)
       .then(response => {
         if (response.status === 200) {
-          console.log("response.data: ", response.data)
           return response.data;
         } else {
           throw new Error("게시글을 불러오는데 문제가 생겼습니다.");
