ead1278e5471d9cc45ecd9415008c0fc34cf294d
byun0-0 <isly7959@naver.com>
2024-07-21 18:51:46 +0900

    feat: 로그인검증 추가

--- src/utils/api/post/createPostRequest.ts
+++ src/utils/api/post/createPostRequest.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const createPostRequest = async (title: string, content: string, category: string) => {
-  const response =  await axios.post('http://localhost:8080/api/v1/community/post', {
+  const response = await axios.post('http://localhost:8080/api/v1/community/post', {
         title: title,
         content: content,
         category: category,
@@ -12,6 +12,10 @@ const createPostRequest = async (title: string, content: string, category: strin
         }
       }
   );
+
+  if (response.data !== undefined && response.data.split('/').pop() === "html>") {
+    console.log(response.data.split('/').pop())
+  }
   return {
     status: response.status,
     location: response.headers.location,
